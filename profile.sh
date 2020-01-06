b489ad9d8c2ce8811633cfd5e8b157a6af8acd05
Matthias Werner <Matthias.Werner1@tu-dresden.de>
2020-01-06 09:38:02 +0100

    changed NR_BODIES in profile.sh

--- profile.sh
+++ profile.sh
@@ -1,8 +1,10 @@
+NR_BODIES=16000
+
 # run profiling on nbody
-nvprof -f -o nbody-100k-timeline.nvvp ./nbody 100000
+nvprof -f -o nbody-100k-timeline.nvvp ./nbody $NR_BODIES
 # obtain metrics for nvvp
-nvprof --analysis-metrics -f -o nbody-100k.nvvp ./nbody 100000
+nvprof --analysis-metrics -f -o nbody-100k.nvvp ./nbody $NR_BODIES
 
 # same for solution binary
-nvprof -f -o nbody-solution-100k-timeline.nvvp ./nbody-solution 100000
-nvprof --analysis-metrics -f -o nbody-solution-100k.nvvp ./nbody-solution 100000
+nvprof -f -o nbody-solution-100k-timeline.nvvp ./nbody-solution $NR_BODIES
+nvprof --analysis-metrics -f -o nbody-solution-100k.nvvp ./nbody-solution $NR_BODIES
